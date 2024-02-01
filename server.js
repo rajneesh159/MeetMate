4efec8fc9a71b96765a28bc4814e73b0e9915044
Rajneesh Kumar verma <99003238+rajneesh159@users.noreply.github.com>
2024-02-01 23:07:48 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const server = require("http").Server(app);
+const { v4: uuidv4 } = require("uuid")
+
+app.use(expres.static("public"));
+app.set("view engine", "ejs")
+
+console.log("server is running");
+
+app.get("/", (req, res) => {
+  res.redirect(`/${uuidv4()}`)
+})
+
+app.get("/:room", (req, res) => {
+  res.render("room", { roomId: req.params.room })
+})
+
+server.listen(3030);
+
